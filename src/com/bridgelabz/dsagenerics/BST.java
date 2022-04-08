4688410ffd3aa2059a476827da4c3a585d288580
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-04-08 22:29:35 +0530

    Added:WELCOME TO DATA STRUCTURE BASED BST PROBLEM USING JAVA GENERICS

---
+++ src/com/bridgelabz/dsagenerics/BST.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.dsagenerics;
+
+public class BST {
+
+	public static void main(String[] args) {
+		System.out.println("WELCOME TO DATA STRUCTURE BASED BST PROBLEM USING JAVA GENERICS");
+	}
+
+}
